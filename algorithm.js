90358a1556e50d2c702062364a4eb2afccf0c8a7
= <=>
2015-02-02 12:41:58 +0100

    adding types of execution

--- algorithm.js
+++ algorithm.js
@@ -1,5 +1,9 @@
 var my = 0;
 var sigma = 1;
+var type = {
+    LAW_OF_LARGE_NUMBERS:1,
+    GET_STOK_VAR:0,
+};
 var generateStokVarFranTathetsFunc = function(func, r, left, right, acc){
     //Integrate to the point we get r
     var currentSum = 0;
@@ -23,14 +27,17 @@ var normalfordelning = function(x){
     return  ((1/(sigma*Math.sqrt(2*Math.PI)))*(Math.pow(Math.E, -((Math.pow((x-my), 2)/(2*(Math.pow(sigma, 2))))))));
     };
 self.addEventListener("message", function(e){
-    var data = e.data;
-    my = data.my;
-    sigma = data.sigma;
-    
-    var stokSum = 0;
-    for(var i = 0; true; i++){
-        var stok = generateStokVarFranTathetsFunc(normalfordelning, Math.random(), my-sigma*10, my+sigma*10, 2*sigma*10/1000000);
-        stokSum = stokSum + stok;
-        postMessage({genom: stokSum/i, ggr: i});
+    if(e.type=type.LAW_OF_LARGE_NUMBERS){
+        var data = e.data;
+        my = data.my;
+        sigma = data.sigma;
+
+        var stokSum = 0;
+        for(var i = 0; true; i++){
+            var stok = generateStokVarFranTathetsFunc(normalfordelning, Math.random(), my-sigma*10, my+sigma*10, 2*sigma*10/1000000);
+            stokSum = stokSum + stok;
+            postMessage({genom: stokSum/i, ggr: i});
+        }
     }
+    
 }, false);
